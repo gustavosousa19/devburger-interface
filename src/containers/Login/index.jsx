c15ce450c45d6fc3bbe6f43d51a68f69b8cd55ed
Gustavo <beckhamgustavo0@gmail.com>
2026-03-04 15:56:21 -0300

    Inicinado a tela de Login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,41 @@
+import Logo from '../../assets/logo.png';
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  Link,
+  RightContainer,
+  Title,
+} from './styles';
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span> Acesse com seu
+          <span>Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Link>Esqueci minha senha.</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link>Não possui conta? Clique aqui.</Link>
+      </RightContainer>
+    </Container>
   );
 }
