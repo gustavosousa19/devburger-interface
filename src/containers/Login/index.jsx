e6cba1064b653c6b3e35f31b6164dfeb1d0c370f
Gustavo <beckhamgustavo0@gmail.com>
2026-03-09 12:53:11 -0300

    Validando o formulário de Login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,3 +1,7 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
 import Logo from '../../assets/logo.png';
 import { Button } from '../../components/Button';
 import {
@@ -10,6 +14,28 @@ import {
 } from './styles';
 
 export function Login() {
+  const schema = yup
+    .object({
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Container>
       <LeftContainer>
@@ -21,17 +47,19 @@ export function Login() {
           <br />
           Acesse com seu <span>Login e senha.</span>
         </Title>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>Email</label>
-            <input type="email" />
+            <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
-            <input type="password" />
+            <input type="password" {...register('password')} />
+            <p>errors?.password?.message</p>
           </InputContainer>
-          <Button>Entrar</Button>
+          <Button type="submit">Entrar</Button>
         </Form>
         <p>
           Não possui conta? <a>Clique aqui.</a>
