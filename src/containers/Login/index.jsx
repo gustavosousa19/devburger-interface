b7d9beae02d557237284139f4e392bcc5f77e1ad
Gustavo <beckhamgustavo0@gmail.com>
2026-03-10 14:48:56 -0300

    concetando com back-end

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 
 import Logo from '../../assets/logo.png';
 import { Button } from '../../components/Button';
+import { api } from '../../services/api';
 import {
   Container,
   Form,
@@ -34,7 +35,14 @@ export function Login() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const response = await api.post('/sessions', {
+      email: data.email,
+      password: data.password,
+    });
+
+    console.log(response);
+  };
 
   return (
     <Container>
