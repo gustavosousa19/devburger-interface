785fa0fb91d7dc8d4334bce72bf2a4f291e39940
Gustavo <beckhamgustavo0@gmail.com>
2026-03-12 17:34:18 -0300

    Tela de Cadastro

---
+++ src/containers/Register/index.jsx
@@ -0,0 +1,103 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+
+import Logo from '../../assets/logo.png';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+import {
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+} from './styles';
+
+export function Register() {
+  const schema = yup
+    .object({
+      name: yup.string().required('O nome é Obrigatório'),
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+        .required('Confirme sua senha'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(errors);
+
+  const onSubmit = async (data) => {
+    const response = await toast.promise(
+      api.post('/users', {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      }),
+      {
+        pending: 'Verificando seus dados',
+        success: 'Cadastro realizado com Sucesso! 👌',
+        error: 'Ops, algo deu errado! Tente novamente 🤯',
+      },
+    );
+
+    console.log(response);
+  };
+
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>Criar Conta</Title>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <label>Nome</label>
+            <input type="text" {...register('name')} />
+            <p>{errors?.name?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Confirmar Senha</label>
+            <input type="password" {...register('confirmPassword')} />
+            <p>{errors?.confirmPassword?.message}</p>
+          </InputContainer>
+          <Button type="submit">Criar Conta</Button>
+        </Form>
+        <p>
+          Já possui conta? <a>Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { Login } from '../containers/Login';
+import { Register } from '../containers/Register';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/cadastro',
+    element: <Register />,
   },
 ]);
