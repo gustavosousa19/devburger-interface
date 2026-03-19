ebb92ff2ae37e9b39f4dfc2005714d82ae4eb902
Gustavo <beckhamgustavo0@gmail.com>
2026-03-19 17:10:16 -0300

    Montando Carrosel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import { api } from '../../services/api';
+import { Container, ContainerItems, Title } from './styles';
 
 export function CategoriesCarousel() {
   const [categories, setCategories] = useState([]);
@@ -16,9 +19,41 @@ export function CategoriesCarousel() {
     loadCategories();
   }, []);
 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1280 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1280, min: 690 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 690, min: 0 },
+      items: 2,
+    },
+  };
+
   return (
-    <div>
-      <h1>Carrosel Categorias</h1>
-    </div>
+    <Container>
+      <Title>Categorias</Title>
+
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisible={false}
+        itemClass="carousel-item"
+      >
+        {categories.map((category) => (
+          <ContainerItems key={category.id} imageUrl={category.url}>
+            <p>{category.name}</p>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
