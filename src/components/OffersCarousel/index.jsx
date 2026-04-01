4e376260635f380445f2bb5a293892f421a1de5b
Gustavo <beckhamgustavo0@gmail.com>
2026-04-01 14:30:11 -0300

    Filtrando Itens

--- src/components/OffersCarousel/index.jsx
+++ src/components/OffersCarousel/index.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
 import { CardProduct } from '../CardProduct';
 import { Container, Title } from './styles';
 
@@ -13,7 +14,12 @@ export function OffersCarousel() {
     async function loadProducts() {
       const { data } = await api.get('/products');
 
-      const onlyOffers = data.filter((product) => product.offer);
+      const onlyOffers = data
+        .filter((product) => product.offer)
+        .map((product) => ({
+          currencyValue: formatPrice(product.price),
+          ...product,
+        }));
 
       setOffers(onlyOffers);
     }
