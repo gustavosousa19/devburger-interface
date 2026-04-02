3e6dd614c77371a781870dcc3b6d5859597244bf
Gustavo <beckhamgustavo0@gmail.com>
2026-04-02 19:10:19 -0300

    Montando a tela de Categorias

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -4,7 +4,6 @@ import { CartButton } from '../CartButton';
 import { CardImage, Container } from './styles';
 
 export function CardProduct({ product }) {
-  console.log(product);
   return (
     <Container>
       <CardImage src={product.url} alt={product.name} />
--- src/containers/Menu/styles.js
+++ src/containers/Menu/styles.js
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Background from '../../assets/background.png';
 import BannerHamburger from '../../assets/banner-hamburger.svg';
 
 export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   background-color: #f0f0f0;
+
+  background:
+    linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)),
+    url('${Background}');
+  background-size: cover;
 `;
 
 export const Banner = styled.div`
@@ -39,6 +46,31 @@ export const Banner = styled.div`
   }
 `;
 
-export const CategoryMenu = styled.div``;
+export const CategoryMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+  margin-top: 30px;
+`;
 
-export const ProductsContainer = styled.div``;
+export const CategoryButton = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+  background: none;
+  color: #9758a6;
+  font-size: 24px;
+  font-weight: 500;
+  padding-bottom: 5px;
+  line-height: 20px;
+  border-bottom: 3px solid #9758a6;
+`;
+
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 40px;
+  gap: 60px;
+  justify-content: center;
+  max-width: 1280px;
+  margin: 50px auto;
+`;
