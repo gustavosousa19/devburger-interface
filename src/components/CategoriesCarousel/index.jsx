7c0719b2b710195c9c9296e7b5c0fe98b1e63ea1
Gustavo <beckhamgustavo0@gmail.com>
2026-04-08 17:04:00 -0300

    Tela de Categorias - pt3

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
+import { useNavigate } from 'react-router-dom';
+
 import { api } from '../../services/api';
-import { Container, ContainerItems, Title } from './styles';
+import { CategoryButton, Container, ContainerItems, Title } from './styles';
 
 export function CategoriesCarousel() {
   const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadCategories() {
@@ -49,7 +52,16 @@ export function CategoriesCarousel() {
       >
         {categories.map((category) => (
           <ContainerItems key={category.id} imageUrl={category.url}>
-            <p>{category.name}</p>
+            <CategoryButton
+              onClick={() => {
+                navigate({
+                  pathname: '/cardapio',
+                  search: `?categoria=${category.id}`,
+                });
+              }}
+            >
+              {category.name}
+            </CategoryButton>
           </ContainerItems>
         ))}
       </Carousel>
