9143751e5555c69a70803dcb920b815276c71a74
Gustavo <beckhamgustavo0@gmail.com>
2026-04-15 00:52:13 -0300

    Criando o Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,42 @@
+import { UserCircleIcon, ShoppingCartIcon } from '@phosphor-icons/react';
+
+import {
+  Container,
+  HeaderLink,
+  LinkContainer,
+  Logout,
+  Navigation,
+  Options,
+  Profile,
+  Content,
+} from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <Navigation>
+          <div>
+            <HeaderLink>Home</HeaderLink>
+            <HeaderLink>Cardápio</HeaderLink>
+          </div>
+        </Navigation>
+        <Options>
+          <Profile>
+            <UserCircleIcon color="#fff" size={24} />
+            <div>
+              <p>
+                Olá, <span>Gustavo</span>
+              </p>
+              <Logout>Sair</Logout>
+            </div>
+          </Profile>
+          <LinkContainer>
+            <ShoppingCartIcon color="#fff" size={24} />
+            <HeaderLink>Carrinho</HeaderLink>
+          </LinkContainer>
+        </Options>
+      </Content>
+    </Container>
+  );
+}
