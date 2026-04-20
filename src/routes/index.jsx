f959d4a296b162442a7fc820647573d7c9a5a729
Gustavo <beckhamgustavo0@gmail.com>
2026-04-20 13:17:00 -0300

    Organizando as Importações

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Footer from '../components/Footer';
 import { Header } from '../components/Header';
-import { Cart } from '../containers/Cart';
-import { Home } from '../containers/Home';
-import { Login } from '../containers/Login';
-import { Menu } from '../containers/Menu';
-import { Register } from '../containers/Register';
+import { Cart, Home, Login, Menu, Register } from '../containers';
 
 export const router = createBrowserRouter([
   {
