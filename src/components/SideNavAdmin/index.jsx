4a81dd3452c747ccdcfc754b897c1dc75d885b32
Gustavo <beckhamgustavo0@gmail.com>
2026-05-12 14:36:50 -0300

    Criando Menu Lateral

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,7 +1,30 @@
+import { SignOut } from '@phosphor-icons/react';
+
+import Logo from '../../assets/logo.png';
+import { useUser } from '../../hooks/UserContext';
+import { navLinks } from './navLinks';
+import { Container, NavLink, NavLinkContainer, Footer } from './styles';
+
 export function SideNavAdmin() {
-    return (
-        <div>
-            <h2>Menu</h2>
-        </div>
-    );
+  const { logout } = useUser();
+
+  return (
+    <Container>
+      <img src={Logo} alt="Hamburger Logo DevBurger" />
+      <NavLinkContainer>
+        {navLinks.map((link) => (
+          <NavLink key={link.id} to={link.path}>
+            {link.icon}
+            <span>{link.label}</span>
+          </NavLink>
+        ))}
+      </NavLinkContainer>
+      <Footer>
+        <NavLink to="/login" onClick={logout}>
+          <SignOut />
+          <span>Sair</span>
+        </NavLink>
+      </Footer>
+    </Container>
+  );
 }
---
+++ src/components/SideNavAdmin/styles.js
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #363636;
+
+  img {
+    width: 60%;
+    margin: 40px 0;
+  }
+`;
+
+export const NavLinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const NavLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 12px 20px;
+  text-decoration: none;
+  color: #ffffff;
+
+  &:hover {
+    background-color: #9758a6;
+  }
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  margin-top: auto;
+`;
