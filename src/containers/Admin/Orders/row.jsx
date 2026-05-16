47a10cc979818a3abdc9b86f82dafbb2481f2a40
Gustavo <beckhamgustavo0@gmail.com>
2026-05-16 14:27:19 -0300

    Criando Menu Superior

--- src/containers/Admin/Orders/row.jsx
+++ src/containers/Admin/Orders/row.jsx
@@ -17,12 +17,25 @@ import { formatDate } from '../../../utils/formatDate';
 import { orderStatusOptions } from './orderStatus';
 import { ProductImage, SelectStatus } from './styles';
 
-export function Row(props) {
-  const { row } = props;
+export function Row(row, setOrders, orders) {
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   async function newStatusOrder(id, status) {
-    await api.put(`orders/${id}`, { status });
+    try {
+      setLoading(true);
+      await api.put(`orders/${id}`, { status });
+
+      const newOrders = orders.map((order) =>
+        order._id === id ? { ...order, status } : order,
+      );
+
+      setOrders(newOrders);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -50,6 +63,7 @@ export function Row(props) {
               (status) => status.value === row.status || null,
             )}
             onChange={(status) => newStatusOrder(row.orderId, status.value)}
+            isLoading={loading}
           />
         </TableCell>
       </TableRow>
@@ -70,7 +84,7 @@ export function Row(props) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.products.map((product) => (
+                  {row.products?.map((product) => (
                     <TableRow key={product.id}>
                       <TableCell component="th" scope="row">
                         {product.id}
@@ -93,6 +107,8 @@ export function Row(props) {
 }
 
 Row.propTypes = {
+  orders: PropTypes.array.isRequired,
+  setOrders: PropTypes.func.isRequired,
   row: PropTypes.shape({
     orderId: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
