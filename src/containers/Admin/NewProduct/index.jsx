ca2453237ce505070b067b111fbb3888263fa124
Gustavo <beckhamgustavo0@gmail.com>
2026-05-20 13:16:05 -0300

    Terminando a Tela de Edição de Produtos

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -2,6 +2,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { Image } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
 
@@ -16,6 +17,7 @@ import {
   Select,
   SubmitButton,
   ErrorMessage,
+  ContainerCheckbox,
 } from './styles';
 
 const schema = yup.object({
@@ -26,6 +28,7 @@ const schema = yup.object({
     .required('Digite o preço do produto')
     .typeError('Digite o preço do produto'),
   category: yup.object().required('Escolha uma categoria'),
+  offer: yup.boolean(),
   file: yup
     .mixed()
     .test('required', 'Escolha um arquivo para continuar', (value) => {
@@ -46,6 +49,8 @@ const schema = yup.object({
 export function NewProduct() {
   const [fileName, setFileName] = useState(null);
   const [categories, setCategories] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadCategories() {
@@ -72,12 +77,17 @@ export function NewProduct() {
     productFormData.append('price', data.price * 100);
     productFormData.append('category_id', data.category.id);
     productFormData.append('file', data.file[0]);
+    productFormData.append('offer', data.offer);
 
     await toast.promise(api.post('/products', productFormData), {
       pending: 'Adicionando Produto',
       success: 'Produto criado com sucesso',
-      error: 'Falha ao adicionar o produto',
+      error: 'Falha ao adicionar o produto, tente novamente',
     });
+
+    setTimeout(() => {
+      navigate('/admin/produtos');
+    }, 2000);
   };
 
   return (
@@ -133,6 +143,13 @@ export function NewProduct() {
             />
 
             <ErrorMessage>{errors?.category?.message}</ErrorMessage>
+          </InputGroup>
+
+          <InputGroup>
+            <ContainerCheckbox>
+              <input type="checkbox" {...register('offer')} />
+              <Label>Produtos em Oferta ?</Label>
+            </ContainerCheckbox>
           </InputGroup>
 
           <SubmitButton>Adicionar Produto</SubmitButton>
